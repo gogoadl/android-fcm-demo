3b8d34a6d3f544f555ef9bb4091ce7a7c557f80c
HyeonWoo Park <gogoadl@naver.com>
2022-10-15 16:02:09 +0900

    initial commit

---
+++ app/src/main/java/com/hyeonwoo/android_fcm_demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hyeonwoo.android_fcm_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
